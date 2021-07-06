1404874d0d6cb14462bad6b13b48dab09883dcbe
Exomanz <74020511+Exomanz@users.noreply.github.com>
2021-07-06 04:19:51 -0500

    Intial Commit

---
+++ BetterStaticLights/Config.cs
@@ -0,0 +1,29 @@
+﻿using IPA.Config.Stores.Attributes;
+using SiraUtil.Converters;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace BetterStaticLights
+{
+    public class Config
+    {
+        public static OpCode opCode1;
+        public static OpCode opCode2;
+
+        [NonNullable, UseConverter(typeof(VersionConverter))]
+        public virtual SemVer.Version Version { get; set; } = new SemVer.Version("0.0.0");
+
+        public virtual string Choice1 { get; set; } = "BackTop";
+        public virtual string Choice2 { get; set; } = "BottomBackSide";
+
+        internal static List<object> LightChoices => new List<object>(5)
+        {
+            "BackTop",
+            "RingLights",
+            "LeftLasers",
+            "RightLasers",
+            "BottomBackSide",
+            "Off"
+        };
+    }
+}
---
+++ BetterStaticLights/HarmonyPatches/BeatmapTransformPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+
+namespace BetterStaticLights.HarmonyPatches
+{
+    [HarmonyPatch(typeof(BeatmapDataNoEnvironmentEffectsTransform))]
+    [HarmonyPatch(nameof(BeatmapDataNoEnvironmentEffectsTransform.CreateTransformedData))]
+    internal static class BeatmapTransformPatch
+    {
+        private static Config Config => Plugin.XConfig;
+
+        [HarmonyTranspiler]
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+
+            codes[5].opcode = Config.opCode1;
+            codes[11].opcode = Config.opCode2;
+
+            return instructions;
+        }
+    }
+}
